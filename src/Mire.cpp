41ed8783a62cfcad92ed81933343257385148d46
DecolleAnais <anais_decolle@orange.fr>
2018-01-31 14:23:16 +0100

    Affichage échiquier

--- src/Mire.cpp
+++ src/Mire.cpp
@@ -6,7 +6,56 @@
 #include "wavefront.h"
 
 Mire::Mire(int row, int col, float squareSize, Transform t): Mesh(GL_TRIANGLES) {
+    // squares black and white
+    bool colorBlack = true;
 
+    float height = row*squareSize;
+    float width = col*squareSize;
+
+    float z = 0.0;
+    
+    int count = 0;
+
+    for(float h = -squareSize;h < height;h += squareSize) {
+        if(count%2 == 0)
+            colorBlack = true;
+        else
+            colorBlack = false;
+
+        count++;
+        for(float w = -squareSize;w < width;w += squareSize) {
+            // change color
+            if(colorBlack) {
+                colorBlack = false;
+                color(Color(0,0,0));
+            }else {
+                colorBlack = true;
+                color(Color(1,1,1));
+            }
+
+            // triangle 1 (left bottom)
+            Point b(w, h, z);
+            Point a(w + squareSize, h, z);
+            Point c(w, h + squareSize, z);
+
+            // triangle 2 (right up)
+            Point e = c;
+            Point d = a;
+            Point f(w + squareSize, h + squareSize, z);
+
+            // create mesh vertices
+            vertex(a.x, a.y, a.z);
+            vertex(b.x, b.y, b.z);
+            vertex(c.x, c.y, c.z);
+
+
+            vertex(d.x, d.y, d.z);
+            vertex(e.x, e.y, e.z);
+            vertex(f.x, f.y, f.z);
+        }
+    }
+    transform = t;
+    /*
     std::vector<vec3> tmp;
     std::vector<int> indices;
 
@@ -38,6 +87,7 @@ Mire::Mire(int row, int col, float squareSize, Transform t): Mesh(GL_TRIANGLES) 
     }
 
     transform = t;
+     */
 }
 
 Object::Object(Transform t, std::string filename, vec3 objectColor) {
